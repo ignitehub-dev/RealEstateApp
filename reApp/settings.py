7157fbcfeeed615401ea2b4b58127d946cc62958
pminniti <pminniti94@gmail.com>
2019-06-18 00:32:47 -0400

    add: prod settings

--- reApp/settings.py
+++ reApp/settings.py
@@ -148,3 +148,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'reapp.testemail@gmail.com'
 EMAIL_HOST_PASSWORD = 'daddy69'
 EMAIL_USE_TLS = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
