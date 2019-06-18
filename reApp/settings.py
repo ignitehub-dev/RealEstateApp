ee7557dd1e84a1b350980da92344a087896a5753
pminniti <pminniti94@gmail.com>
2019-06-17 23:46:00 -0400

    fix

--- reApp/settings.py
+++ reApp/settings.py
@@ -146,5 +146,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'reapp.testemail@gmail.com'
-EMAIL_HOST_PASSWORD = 'minniti94'
+EMAIL_HOST_PASSWORD = 'daddy69'
 EMAIL_USE_TLS = True
